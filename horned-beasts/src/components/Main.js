341bf4d6cdd1b0d447b345f7c253e87727e0c151
Ahmad-Khaled-Zaid <ahmadk.zaid97@gmail.com>
2021-08-29 19:16:59 +0300

    upload the class-01

---
+++ horned-beasts/src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HornedBeasts from './HornedBeasts';
+class Main extends React.Component{
+render(){
+    return(
+        <main>
+           <HornedBeasts/>
+        </main>
+    )
+}
+}
+export default Main;
