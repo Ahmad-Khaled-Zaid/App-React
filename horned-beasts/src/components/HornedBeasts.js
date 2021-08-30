ab5a4d66a1283a1a9bec89f601d45c4bfdfa412a
Ahmad-Khaled-Zaid <ahmadk.zaid97@gmail.com>
2021-08-30 13:54:51 +0300

    fix the errors

--- horned-beasts/src/components/HornedBeasts.js
+++ horned-beasts/src/components/HornedBeasts.js
@@ -3,8 +3,9 @@ class HornedBeasts extends React.Component {
     render() {
         return (
             <div>
-                <h2>UniWhal</h2>
-                <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="" /> 
+                <h2>{this.props.title}</h2>
+                <img src={this.props.image} alt="" />
+                <p>{this.props.description}</p>
             </div>
 
         )
