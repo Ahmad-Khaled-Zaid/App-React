91d96e5d6826dc0fde9715edfe8a76371fc5bb35
Ahmad-Khaled-Zaid <ahmadk.zaid97@gmail.com>
2021-08-30 19:50:46 +0300

    solve class 02

--- horned-beasts/src/components/HornedBeasts.js
+++ horned-beasts/src/components/HornedBeasts.js
@@ -1,16 +1,70 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card'
+import JsonData from '../assest/data.json'
 class HornedBeasts extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            votes: 0,
+
+        }
+    }
+
+    addVotes = () => {
+        this.setState({
+            votes: this.state.votes + 1
+
+        })
+    }
     render() {
+
+
         return (
             <div>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.image} alt="" />
-                <p>{this.props.description}</p>
+                {JsonData.map((data) => {
+                    return (
+
+                        <div>
+                            {/* <h1>{data.title}</h1>
+                            <p>{data.description}</p>
+                            <img src={data.image_url} alt="" onClick={this.addVotes} />
+                            <h1>{this.state.votes} ❤️</h1> */}
+
+                            <Card style={{ width: '30rem'} }>
+                                <Card.Img variant="top" src={data.image_url} onClick={this.addVotes} />
+                                <Card.Body>
+                                    <Card.Title>{data.title}</Card.Title>
+                                    <Card.Text>
+                                    {data.description}
+                                    </Card.Text>
+                                    <Card.Text>
+                                    {this.state.votes} ❤️
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+
+                    )
+                })}
+
             </div>
 
+
         )
+
+
+
+
+
+
+
     }
 
 }
+
+// console.log(this.addVotes);
+
+
 export default HornedBeasts;
 
