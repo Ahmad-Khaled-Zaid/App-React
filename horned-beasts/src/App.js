9a8155ed53fcfba7a1768594d48d011b58e4ab3e
Ahmad-Khaled-Zaid <ahmadk.zaid97@gmail.com>
2021-09-01 16:03:12 +0300

    complete lab 3

--- horned-beasts/src/App.js
+++ horned-beasts/src/App.js
@@ -1,48 +1,47 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import JsonData from '../assest/data.json'
-
 import React from 'react';
-import Header from './components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 import Main from './components/Main';
-import Footer from './components/Footer'
-import DataModel from './components/DataModel';
-
+import Footer from './components/Footer';
+import JSONDATA from './assest/data.json';
+import DataModal from './components/DataModel';
 
 class App extends React.Component {
-
   constructor(props) {
     super(props)
     this.state = {
-      show: false
+      selectedBeast: JSONDATA,
+      showModel: false
     }
-
   }
-  handleClose = () => {
-    this.setState({ show: !this.state.show })
-
+  alertModal = (title) => {
+    const selectedBeast2 = JSONDATA.filter((item)  => { 
+      return (item.title === title);
+    });
+    console.log(selectedBeast2);
+    this.setState({
+      showModel: true,
+      selectedBeast: selectedBeast2[0],
+    })
+  }
+  closeModal = () => {
+    this.setState({
+      showModel: false
+    })
   }
   render() {
     return (
       <div>
         <Header />
-        <Main handleClose={this.handleClose}/>
+        <Main  HornedData={JSONDATA} displayModal={this.alertModal}  />
+        <DataModal
+          selectedBeast={this.state.selectedBeast}
+          show={this.state.showModel}
+          close={this.closeModal}
+        />
         <Footer />
-        <DataModel show={this.state.show} />
       </div>
-
-
-
-
-
-
-
-
-
-
-
     )
   }
 }
-
 export default App;
